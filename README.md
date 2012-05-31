e7e2597a615621d4acd0a96e19735dd94241e688
SixArm <sixarm@sixarm.com>
2012-05-30 18:33:11 -0700

    Publish

---
+++ README.md
@@ -0,0 +1,105 @@
+# SixArm.com » Ruby » <br> Numeric rounding methods
+
+* Doc: <http://sixarm.com/sixarm_ruby_numeric_round/doc>
+* Gem: <http://rubygems.org/gems/sixarm_ruby_numeric_round>
+* Repo: <http://github.com/sixarm/sixarm_ruby_numeric_round>
+* Email: Joel Parker Henderson, <joel@sixarm.com>
+
+
+## Introduction
+
+Simple numeric rounding methods like #round, #floor, #ceil with various precisions.
+
+For docs go to <http://sixarm.com/sixarm_ruby_numeric_round/doc>
+
+Want to help? We're happy to get pull requests.
+
+
+## Install quickstart
+
+Install:
+
+    gem install sixarm_ruby_numeric_round
+
+Bundler:
+
+    gem "sixarm_ruby_numeric_round", "~>1.0.0"
+
+Require:
+
+    require "sixarm_ruby_numeric_round"
+
+
+## Install with security (optional)
+
+To enable high security for all our gems:
+
+    wget http://sixarm.com/sixarm.pem
+    gem cert --add sixarm.pem
+    gem sources --add http://sixarm.com
+
+To install with high security:
+
+    gem install sixarm_ruby_numeric_round --test --trust-policy HighSecurity
+
+
+## Examples
+
+Round at a given decimal point:
+
+    4.555.ceil_at(1)  #=> 4.6
+    4.555.floor_at(1)  #=> 4.5
+    4.555.round_at(1)  #=> 4.6
+
+Round to a given precision:
+
+    4.555.ceil_to(0.1)   #=> 4.6
+    4.555.floor_to(0.1)  #=> 4.5
+    4.555.round_to(0.1)  #=> 4.6
+
+Conceptually, the methods for round, floor, and ceil will typically be for floating point numbers.
+However, these methods can actually be for pretty much any Numeric object.
+For example, one could round an Integer to the nearest kilo.
+
+
+## Credit
+
+These methods are based on the Facets library for Ruby and the developer named Trans. 
+
+The #round_at and #round_to methods are exact copies and fully compatible with Facets. The rest of the methods are very similar code and fully co-existant with Facets. 
+
+We're asking the Facets team to consider including the rest of the methods.
+
+
+## Changes
+
+* 2012-05-30 1.0.0 Create baesd on Facets methods by Trans.
+
+
+## License
+
+You may choose any of these open source licenses:
+
+  * Apache License
+  * BSD License
+  * CreativeCommons License, Non-commercial Share Alike
+  * GNU General Public License Version 2 (GPL 2)
+  * GNU Lesser General Public License (LGPL)
+  * MIT License
+  * Perl Artistic License
+  * Ruby License
+
+The software is provided "as is", without warranty of any kind,
+express or implied, including but not limited to the warranties of
+merchantability, fitness for a particular purpose and noninfringement.
+
+In no event shall the authors or copyright holders be liable for any
+claim, damages or other liability, whether in an action of contract,
+tort or otherwise, arising from, out of or in connection with the
+software or the use or other dealings in the software.
+
+This license is for the included software that is created by SixArm;
+some of the included software may have its own licenses, copyrights,
+authors, etc. and these do take precedence over the SixArm license.
+
+Copyright (c) 2005-2012 Joel Parker Henderson
---
+++ lib/sixarm_ruby_numeric_round.rb
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+=begin rdoc
+Please see README
+=end
+
+class Numeric
+
+  # Conceptually, ceil is expected to apply to floating point numbers.
+  # However it can actually be applied to pretty much any Numeric object.
+  # For example, one could ceil an Integer to the nearest kilo.
+  #
+  # See Float#ceil_at.
+
+  def ceil_at(*args)
+    to_f.ceil_at(*args)
+  end
+
+  # See Float#ceil_to.
+
+  def ceil_to(*args)
+    to_f.ceil_to(*args)
+  end
+
+  # Conceptually, floor is expected to apply to floating point numbers.
+  # However it can actually be applied to pretty much any Numeric object.
+  # For example, one could floor an Integer to the nearest kilo.
+  #
+  # See Float#floor_at.
+
+  def floor_at(*args)
+    to_f.floor_at(*args)
+  end
+
+  # See Float#floor_to.
+
+  def floor_to(*args)
+    to_f.floor_to(*args)
+  end
+
+  # Conceptually, round is expected to apply to floating point numbers.
+  # However it can actually be applied to pretty much any Numeric object.
+  # For example, one could round an Integer to the nearest kilo.
+  #
+  # See Float#round_at.
+
+  def round_at(*args)
+    to_f.round_at(*args)
+  end
+
+  # See Float#round_to.
+
+  def round_to(*args)
+    to_f.round_to(*args)
+  end
+
+end
+
+class Float
+
+  # Ceil to the given decimal position.
+  #
+  #   4.555.ceil_at(0)  #=> 5.0
+  #   4.555.ceil_at(1)  #=> 4.6
+  #   4.555.ceil_at(2)  #=> 4.56
+  #   4.555.ceil_at(3)  #=> 4.555
+  #
+  # CREDIT: Trans & Joel Parker Henderson
+
+  def ceil_at( d ) #d=0
+    (self * (10.0 ** d)).ceil.to_f / (10.0 ** d)
+  end
+
+  # Ceils to the nearest _n_th degree.
+  #
+  #   4.555.ceil_to(1)     #=> 5.0
+  #   4.555.ceil_to(0.1)   #=> 4.6
+  #   4.555.ceil_to(0.01)  #=> 4.56
+  #   4.555.ceil_to(0)     #=> 4.555
+  #
+  # CREDIT: Trans & Joel Parker Henderson
+
+  def ceil_to( n ) #n=1
+    return self if n == 0
+    (self * (1.0 / n)).ceil.to_f / (1.0 / n)
+  end
+
+  # Floor to the given decimal position.
+  #
+  #   4.555.floor_at(0)  #=> 4.0
+  #   4.555.floor_at(1)  #=> 4.5
+  #   4.555.floor_at(2)  #=> 4.55
+  #   4.555.floor_at(3)  #=> 4.555
+  #
+  # CREDIT: Trans & Joel Parker Henderson
+
+  def floor_at( d ) #d=0
+    (self * (10.0 ** d)).floor.to_f / (10.0 ** d)
+  end
+
+  # Floors to the nearest _n_th degree.
+  #
+  #   4.555.floor_to(1)     #=> 4.0
+  #   4.555.floor_to(0.1)   #=> 4.5
+  #   4.555.floor_to(0.01)  #=> 4.55
+  #   4.555.floor_to(0)     #=> 4.555
+  #
+  # CREDIT: Trans & Joel Parker Henderson
+
+  def floor_to( n ) #n=1
+    return self if n == 0
+    (self * (1.0 / n)).floor.to_f / (1.0 / n)
+  end
+
+  # Round to the given decimal position.
+  #
+  #   4.555.round_at(0)  #=> 5.0
+  #   4.555.round_at(1)  #=> 4.6
+  #   4.555.round_at(2)  #=> 4.56
+  #   4.555.round_at(3)  #=> 4.555
+  #
+  # CREDIT: Trans & Joel Parker Henderson
+
+  def round_at( d ) #d=0
+    (self * (10.0 ** d)).round.to_f / (10.0 ** d)
+  end
+
+  # Rounds to the nearest _n_th degree.
+  #
+  #   4.555.round_to(1)     #=> 5
+  #   4.555.round_to(0.1)   #=> 4.6
+  #   4.555.round_to(0.01)  #=> 4.56
+  #   4.555.round_to(0)     #=> 4.555
+  #
+  # CREDIT: Trans & Joel Parker Henderson
+
+  def round_to( n ) #n=1
+    return self if n == 0
+    (self * (1.0 / n)).round.to_f / (1.0 / n)
+  end
+
+end
